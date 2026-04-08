f416276153dccb7881d8607f64844becaf3c5348
Paul Opara <74050900+Paul-3792@users.noreply.github.com>
2026-04-08 12:03:08 +0100

    updated footer link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,9 +9,9 @@ export default function Footer() {
       <div className={styles.container}>
         <p>© {currentYear} Paul Opara. All Rights Reserved.</p>
         <div className={styles.socials}>
-          <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">GitHub</a>
-          <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="mailto:youremail@example.com">Email</a>
+          <a href="https://github.com/Paul-3792" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://www.linkedin.com/in/paul-opara-280b81195/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="mailto:opara1487@gmail.com">Email</a>
         </div>
       </div>
     </footer>
